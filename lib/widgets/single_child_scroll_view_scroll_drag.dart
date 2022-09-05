43d483a147a7d28c909795746dabc9232d18ac99
Miguel Angel Besalduch Garcia <miguel.besalduch@indulgeme.app>
2022-09-05 18:28:59 -0300

    Upgrade to Flutter 3.3 & increase library version number

--- lib/widgets/single_child_scroll_view_scroll_drag.dart
+++ lib/widgets/single_child_scroll_view_scroll_drag.dart
@@ -4,17 +4,20 @@ import 'scroll_configuration_drag_mouse.dart';
 class SingleChildScrollViewScrollDrag extends StatelessWidget {
   final Widget child;
   final Axis scrollDirection;
+  final _controller = ScrollController();
 
-  const SingleChildScrollViewScrollDrag(
+  SingleChildScrollViewScrollDrag(
       {Key? key, required this.child, this.scrollDirection = Axis.vertical})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) => Scrollbar(
       thumbVisibility: false,
+      controller: _controller,
       child: ScrollConfiguration(
           behavior: ScrollConfigurationDragMouse(),
           child: SingleChildScrollView(
+            controller: _controller,
             scrollDirection: scrollDirection,
             child: child,
           )));
