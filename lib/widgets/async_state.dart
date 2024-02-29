56cc005cb4f7afdd1211a8e8dedbba52b1f15bc8
Miguel Angel Besalduch Garcia <mabg.dev@gmail.com>
2024-02-29 17:18:04 -0300

    AsyncState uses AsyncFactory to create async controllers & AsyncStateWithControllerDisposable

--- lib/widgets/async_state.dart
+++ lib/widgets/async_state.dart
@@ -35,9 +35,8 @@ abstract class StateWithController<T extends StatefulWidget, U>
 /// When asyncInitState is done, it examines the Future result. If all is ok,
 /// the buildWhenDone is called.
 ///
-/// If an error occurs, the error is passed to ToastService
-/// and the buildWhenError method is called. By default, buildWhenError shows in
-/// the middle a red replay icon, that can touch to retry.
+/// If an error occurs, buildWhenError method is called.
+/// By default, buildWhenError shows in the middle a red replay icon, that can touch to retry.
 abstract class AsyncState<T extends StatefulWidget> extends State<T> {
   bool _futureDone = false;
 
@@ -47,15 +46,21 @@ abstract class AsyncState<T extends StatefulWidget> extends State<T> {
   /// Build method when the [State] are initialized correctly with [asyncInitState]
   Widget buildWhenDone(BuildContext context);
 
-  static Function(Object error)? onError;
+  static void Function(Object error)? onError;
 
   Completer<void>? _asyncInitStateCompleter;
 
   @override
   void initState() {
-    _asyncInitStateCompleter = Completer<void>();
-    asyncInitState().then((_) => _asyncInitStateCompleter!.complete());
+    _asyncInitState();
     super.initState();
+  }
+
+  void _asyncInitState() {
+    _asyncInitStateCompleter = Completer<void>();
+    unawaited(asyncInitState().then((_) => _asyncInitStateCompleter!.complete(),
+        onError: (Object err, StackTrace? stack) =>
+            _asyncInitStateCompleter!.completeError(err, stack)));
   }
 
   @override
@@ -85,45 +90,138 @@ abstract class AsyncState<T extends StatefulWidget> extends State<T> {
   /// Build method when [asyncInitState] ends with and error. It shows in
   /// the middle a red replay icon, that can touch to retry.
   Widget buildWhenError(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: IconButton(
-              icon: Stack(children: [
-                Container(),
-                const Positioned(
-                  left: 8,
-                  top: 3,
-                  child: Icon(
-                    Icons.replay,
-                    color: Colors.blueAccent,
+    return SizedBox.expand(
+      child: Center(
+        child: IconButton(
+            icon: const Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.error_outline,
+                    color: Colors.red,
                   ),
-                ),
-                const Icon(
-                  Icons.error,
-                  color: Colors.red,
-                  size: 15,
-                )
-              ]),
-              // ignore: no-empty-block
-              onPressed: () => {setState(() {})})),
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Icon(
+                      Icons.replay,
+                      color: Colors.white,
+                      size: 42,
+                    ),
+                  )
+                ]),
+            onPressed: () => {setState(_asyncInitState)}),
+      ),
     );
   }
 }
 
-/// Define an [AsyncState] that will create a future controller.
-/// The controller could be used by child objects via [controller] property.
+/// [AsyncFactory.futureCancelable] function for wrapping
+/// functions that return [Future] that must be supported for canceling.
+typedef FutureCancelable = Future<U> Function<U>(Future<U> f);
+
+class _CancelException {}
+
+/// Base class for creating async controller factories.
+/// To create an async controller factory, extends this class and overrides
+/// [AsyncFactory.instance].
+/// Inside instance, create the code for creating the controller.
+/// It could have async calls, for exemple a call to the server.
+/// Put these async calls wrapped with futureCancelable. This allows to stop
+/// automatically the creation if cancel is activated.
+///
+/// {@tool snippet}
+/// This sample shows an async controller factory.
+/// It uses the method OrdersOpenApi().orders, that returns a Future.
+/// See how the call is wrapped with futureCancelable and how it is passed to
+/// orders.
+/// Inside orders, the method ordersOpen returns a Future. So, again, is wrapped
+/// with futureCancelable.
+/// If, for example, the user is in a screen and this controller is awaiting in
+/// ordersOpen and the user backs, the factory is cancelled. When ordersOpen
+/// ends the Future, it doesn't execute the next line, ordersOpen.orders.map,
+/// neither the line orders.sort inside instance.
+///
+/// ```dart
+/// class OrdersOpenControllerFactory extends AsyncFactory<OrdersOpenController> {
+/// @override
+///  Future<OrdersOpenController> instance() async {
+///    final List<Order> orders =
+///        await futureCancelable(OrdersOpenApi().orders(futureCancelable));
+///    orders.sort((order1, order2) => order1.start.compareTo(order2.start));
+///    return OrdersOpenController._(orders);
+/// }
+///
+/// class OrdersOpenApi {
+///  Future<List<Order>> orders(FutureCancelable futureCancelable) async {
+///    final OrdersOpenRequest request = OrdersOpenRequest();
+///    final OrdersOpenResponse ordersOpen =
+///        await futureCancelable(Server.instance.ordersOpen(request));
+///    final List<Order> result =
+///        ordersOpen.orders.map<Order>((OrderOpenResponse apiOrder) {
+///         ...
+///
+/// ```
+/// {@end-tool}
+abstract class AsyncFactory<T> {
+  bool _isCanceled = false;
+
+  /// True if must cancel the creation of the current async controller.
+  bool get isCanceled => _isCanceled;
+
+  /// Checks if the factory must cancel the creation of the current async
+  /// controller. If true, it stops the creation.
+  Future<U> futureCancelable<U>(Future<U> f) async {
+    return f.then(
+      (value) {
+        if (_isCanceled) {
+          throw _CancelException();
+        } else {
+          return value;
+        }
+      },
+    );
+  }
+
+  Future<T?> _create() async {
+    try {
+      return await futureCancelable(instance());
+    } on _CancelException {
+      return null;
+    }
+  }
+
+  /// Must return the future of controller.
+  Future<T> instance();
+
+  /// Mark like must cancel the creation of the current async controller.
+  void cancel() {
+    _isCanceled = true;
+  }
+}
+
+/// Define an [AsyncState] that depends on async controller.
+/// An [AsyncFactory] of the controller must be returned. It's used to create
+/// the async controller.
+/// If the widget is disposed before the controller is created, [AsyncFactory.cancel]
+/// is called.
+/// When the controller is created, could be used via [controller] property.
 /// This controller could be mocked by [MockFactory].
 abstract class AsyncStateWithController<T extends StatefulWidget, U>
     extends AsyncState<T> {
   U? _controller;
   U get controller => _controller!;
+  AsyncFactory<U>? _factory;
 
-  Future<U> createController();
+  @protected
+  AsyncFactory<U> controllerFactory();
 
   @override
   @mustCallSuper
   Future<void> asyncInitState() async {
-    _controller = await (mock?.of<Future<U>>() ?? createController());
+    _factory = mock?.of<AsyncFactory<U>>() ?? controllerFactory();
+    _controller = await _factory!._create();
+    _factory = null;
   }
 
   /// Determine if the controller is created. CreateController method
@@ -131,4 +229,10 @@ abstract class AsyncStateWithController<T extends StatefulWidget, U>
   /// when calls another methods, like buildWhenLoading, buildWhenError,
   /// dispose, ...
   bool isControllerCreated() => _controller != null;
+
+  @override
+  void dispose() {
+    _factory?.cancel();
+    super.dispose();
+  }
 }
--- lib/widgets/async_state_with_controller_disposable.dart
+++ lib/widgets/async_state_with_controller_disposable.dart
@@ -5,14 +5,14 @@ import 'package:flutter/widgets.dart';
 import 'async_state.dart';
 
 /// Defines an interface indicating that an object has the dispose method.
-abstract class Disposable {
+abstract interface class Disposable {
   Future<void> dispose();
 }
 
 /// Define an [AsyncState] that will create a future [Disposable] controller.
 /// When the object is disposed, it checks if the controller is created. In
 /// this case, it calls [dispose] like an unawaited future.
-/// The controller could be used by child objects via [controller] property.
+/// The controller could be used via [controller] property.
 /// This controller could be mocked by [MockFactory].
 abstract class AsyncStateWithControllerDisposable<T extends StatefulWidget,
     U extends Disposable> extends AsyncStateWithController<T, U> {
